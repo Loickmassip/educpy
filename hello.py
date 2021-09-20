d9822d13aa92acdc179e7ff3739001f25486692a
David Mourereau <david.mourereau@gmail.com>
2021-09-20 23:40:01 +0200

    ajout de la fonction enregistrement resultat dans fichier.

---
+++ hello.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+
+def record_result(result):
+    file = open("result.txt", "w")
+    today=datetime.now()
+    strdate=today.strftime("%d-%b-%Y (%H:%M:%S.%f)") + " " + result
+    file.write(strdate)
+
+
+if __name__ == "__main__":
+  print("Welcome to EducPy Calculator, please enter your values")
+  # call record_result once calculation is done  
+  
+
+
